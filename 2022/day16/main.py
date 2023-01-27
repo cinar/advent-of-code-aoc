e72f82927bc5636e392a4e27b52c1e0112204d58
Onur Cinar <onur.cinar@gmail.com>
2023-01-27 17:12:22 +0000

    AoC 2022 Day 16 Part 2 Not Working.

--- 2022/day16/main.py
+++ 2022/day16/main.py
@@ -33,5 +33,31 @@ def dp1(name, minutes, opened):
 
   return max(option1, option2)
 
+@functools.lru_cache(maxsize=1024)
+def dp2(name, minutes, opened):
+  if minutes <= 0:
+    return 0
 
-print("p1", dp1("AA", 30, frozenset()))
+  valve = valves[name]
+
+  option0 = dp1("AA", 26, opened)
+  
+  option1 = max(dp2(n, minutes - 1, opened) for n in valve.tunnels)
+  if valve.rate == 0 or name in opened:
+    return max(option0, option1)
+    
+  opened = opened.union({name})
+  option2 = (valve.rate * (minutes - 1)) + max(
+    dp2(n, minutes - 2, opened) for n in valve.tunnels)
+
+  return max(option0, option1, option2)
+
+def p1():
+  print("p1", dp1("AA", 30, frozenset()))
+
+
+def p2():
+  print("p2", dp2("AA", 26, frozenset()))
+
+
+p2()
